5016b9cf9e9556667134e9eb916ef8f4d9678793
Lisandro Crespo <hey@lisandroct.com>
2020-10-19 18:30:14 -0300

    Update package version

--- Runtime/Activators.cs
+++ Runtime/Activators.cs
@@ -13,14 +13,14 @@ namespace RishUI
             
             if(type != returnType && !type.IsSubclassOf(typeof(T)))
             {
-                throw new ArgumentException("type must be subtype of T");
+                throw new ArgumentException($"{type.FullName} isn't a subtype of {returnType.FullName}");
             }
             
             var constructorInfo = type.GetConstructor(Type.EmptyTypes);
 
             if (constructorInfo == null)
             {
-                throw new ArgumentException("No parameterless constructor found.");
+                throw new ArgumentException($"No parameterless constructor found for {type.FullName}.");
             }
             
             var expression = Expression.New(constructorInfo);
