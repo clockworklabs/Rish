c82f6e154d327c2877d361e137a04d2560f208fb
Lisandro Crespo <hey@lisandroct.com>
2020-10-24 20:22:40 -0300

    Add null RishTransform

--- Runtime/Real/RishTransform.cs
+++ Runtime/Real/RishTransform.cs
@@ -14,10 +14,15 @@ namespace RishUI
         public Vector2 scale;
         public float rotation;
 
+        public static readonly RishTransform Zero = default;
         public static readonly RishTransform Default = new RishTransform
         {
             max = Vector2.one,
             scale = Vector2.one
+        };
+        public static readonly RishTransform Null = new RishTransform
+        {
+            top = float.NaN
         };
         
         public static RishTransform operator *(RishTransform a, RishTransform b)
@@ -40,6 +45,48 @@ namespace RishUI
             return (parentSize * (max - min) - new Vector2(left + right, top + bottom)) * scale;
         }
 
+        public bool IsValid()
+        {
+            if (float.IsNaN(top) || float.IsInfinity(top))
+            {
+                return false;
+            }
+            if (float.IsNaN(left) || float.IsInfinity(left))
+            {
+                return false;
+            }
+            if (float.IsNaN(bottom) || float.IsInfinity(bottom))
+            {
+                return false;
+            }
+            if (float.IsNaN(right) || float.IsInfinity(right))
+            {
+                return false;
+            }
+            if (float.IsNaN(min.x) || float.IsInfinity(min.x))
+            {
+                return false;
+            }
+            if (float.IsNaN(min.y) || float.IsInfinity(min.y))
+            {
+                return false;
+            }
+            if (float.IsNaN(rotation) || float.IsInfinity(rotation))
+            {
+                return false;
+            }
+            if (float.IsNaN(scale.x) || float.IsInfinity(scale.x))
+            {
+                return false;
+            }
+            if (float.IsNaN(scale.y) || float.IsInfinity(scale.y))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
         public override string ToString()
         {
             return $"{min} - {max} - {top} - {left} - {bottom} - {right} - {scale}";
@@ -47,6 +94,19 @@ namespace RishUI
 
         public bool Equals(RishTransform other)
         {
+            var isValid = IsValid();
+            var otherIsValid = other.IsValid();
+            
+            if (isValid != otherIsValid)
+            {
+                return false;
+            }
+
+            if (!isValid)
+            {
+                return true;
+            }
+            
             if(!Mathf.Approximately(min.x, other.min.x))
             {
                 return false;
