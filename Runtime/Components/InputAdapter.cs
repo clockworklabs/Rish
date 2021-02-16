ba1eacc07795011f49b609493105deed554d542d
lisandroct <lisandroct@gmail.com>
2021-02-16 09:54:34 -0600

    Add InputAdapter

---
+++ Runtime/Components/InputAdapter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RishUI.Input;
+using UnityEngine;
+
+namespace RishUI.Components
+{
+    public class InputAdapter : RishComponent<InputAdapterProps>, IHoverStartListener, IHoverEndListener, ITapListener, ITapStartListener, ITapCancelListener, IDragListener, IDragStartListener, IDragEndListener, IScrollListener
+    {
+        protected override RishElement Render()
+        {
+            return Props.content;
+        }
+
+        public void OnHoverStart(HoverInfo info) => Props.onHoverStart?.Invoke(info);
+
+        public void OnHoverEnd(HoverInfo info) => Props.onHoverEnd?.Invoke(info);
+
+        public bool OnTap(TapInfo info) => Props.onTap?.Invoke(info) ?? false;
+
+        public bool OnTapStart(TapInfo info) => Props.onTapStart?.Invoke(info) ?? false;
+
+        public bool OnTapCancel(TapInfo info) => Props.onTapCancel?.Invoke(info) ?? false;
+
+        public bool OnDrag(DragInfo info) => Props.onDrag?.Invoke(info) ?? false;
+
+        public bool OnDragStart(DragInfo info) => Props.onDragStart?.Invoke(info) ?? false;
+
+        public bool OnDragEnd(DragInfo info) => Props.onDragEnd?.Invoke(info) ?? false;
+
+        public bool OnScroll(ScrollInfo info) => Props.onScroll?.Invoke(info) ?? false;
+    }
+    
+    [Serializable]
+    public struct InputAdapterProps : IRishData<InputAdapterProps>
+    {
+        public Action<HoverInfo> onHoverStart;
+        public Action<HoverInfo> onHoverEnd;
+        public Func<TapInfo, bool> onTap;
+        public Func<TapInfo, bool> onTapStart;
+        public Func<TapInfo, bool> onTapCancel;
+        public Func<DragInfo, bool> onDrag;
+        public Func<DragInfo, bool> onDragStart;
+        public Func<DragInfo, bool> onDragEnd;
+        public Func<ScrollInfo, bool> onScroll;
+
+        public RishElement content;
+
+        public void Default() { }
+
+        public bool Equals(InputAdapterProps other)
+        {
+            if (onHoverStart != null || other.onHoverStart != null)
+            {
+                return false;
+            }
+            if (onHoverEnd != null || other.onHoverEnd != null)
+            {
+                return false;
+            }
+            if (onTap != null || other.onTap != null)
+            {
+                return false;
+            }
+            if (onTapStart != null || other.onTapStart != null)
+            {
+                return false;
+            }
+            if (onTapCancel != null || other.onTapCancel != null)
+            {
+                return false;
+            }
+            if (onDrag != null || other.onDrag != null)
+            {
+                return false;
+            }
+            if (onDragStart != null || other.onDragStart != null)
+            {
+                return false;
+            }
+            if (onDragEnd != null || other.onDragEnd != null)
+            {
+                return false;
+            }
+            if (onScroll != null || other.onScroll != null)
+            {
+                return false;
+            }
+
+            if (!content.Equals(other.content))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
