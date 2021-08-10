75261d173e8e96ce0be66f14eaa4e09df5a98f4a
Lisandro Crespo <lisandroct@gmail.com>
2021-08-10 09:46:44 -0300

    Add filled type to images

--- Runtime/Components/Image.cs
+++ Runtime/Components/Image.cs
@@ -40,7 +40,12 @@ namespace RishUI.Components
                     color = color,
                     maskable = settings.maskable,
                     raycastTarget = settings.raycastTarget,
-                    type = State.sprite != null && State.sprite.border != Vector4.zero ? UnityImageDefinition.Type.Sliced : UnityImageDefinition.Type.Simple,
+                    type = settings.filled
+                        ? UnityImageDefinition.Type.Filled 
+                        : State.sprite != null && State.sprite.border != Vector4.zero
+                            ? UnityImageDefinition.Type.Sliced 
+                            : UnityImageDefinition.Type.Simple,
+                    fill = settings.fill,
                     preserveAspectRatio = settings.preserveAspectRatio
                 }
             });
@@ -61,61 +66,6 @@ namespace RishUI.Components
             var state = State;
             state.sprite = sprite;
             State = state;
-        }
-    }
-
-    public struct ImageSettings : IEquatable<ImageSettings>
-    {
-        public static readonly ImageSettings Default = new ImageSettings
-        {
-            color = Color.white,
-            maskable = true,
-            raycastTarget = true
-        };
-        
-        public Color color;
-        public bool maskable;
-        public bool raycastTarget;
-        public bool preserveAspectRatio;
-
-        public ImageSettings(ImageSettings other)
-        {
-            color = other.color;
-            maskable = other.maskable;
-            raycastTarget = other.raycastTarget;
-            preserveAspectRatio = other.preserveAspectRatio;
-        }
-        
-        public bool Equals(ImageSettings other)
-        {            
-            if (raycastTarget != other.raycastTarget)
-            {
-                return false;
-            }
-
-            if (maskable != other.maskable)
-            {
-                return false;
-            }
-
-            if (preserveAspectRatio != other.preserveAspectRatio)
-            {
-                return false;
-            }
-
-            var transparent = Mathf.Approximately(color.a, 0);
-            if(transparent != Mathf.Approximately(other.color.a, 0))
-            {
-                return false;
-            }
-
-            if (!transparent && (!Mathf.Approximately(color.r, other.color.r) || !Mathf.Approximately(color.g, other.color.g) ||
-                                 !Mathf.Approximately(color.b, other.color.b) || !Mathf.Approximately(color.a, other.color.a)))
-            {
-                return false;
-            }
-
-            return true;
         }
     }
     
@@ -164,5 +114,176 @@ namespace RishUI.Components
         public void Default() { }
 
         public bool Equals(ImageState other) => sprite == other.sprite;
+    }
+
+    public struct ImageSettings : IEquatable<ImageSettings>
+    {
+        public Color color;
+        public bool maskable;
+        public bool raycastTarget;
+        public bool preserveAspectRatio;
+        public bool filled;
+        public ImageFill fill;
+        
+        public static readonly ImageSettings Default = new ImageSettings
+        {
+            color = Color.white,
+            maskable = true,
+            raycastTarget = true
+        };
+
+        public ImageSettings(ImageSettings other)
+        {
+            color = other.color;
+            maskable = other.maskable;
+            raycastTarget = other.raycastTarget;
+            preserveAspectRatio = other.preserveAspectRatio;
+            filled = other.filled;
+            fill = other.fill;
+        }
+        
+        public bool Equals(ImageSettings other)
+        {            
+            if (raycastTarget != other.raycastTarget)
+            {
+                return false;
+            }
+
+            if (maskable != other.maskable)
+            {
+                return false;
+            }
+
+            if (preserveAspectRatio != other.preserveAspectRatio)
+            {
+                return false;
+            }
+
+            if (filled)
+            {
+                if (!other.filled || !fill.Equals(other.fill))
+                {
+                    return false;
+                }
+            }
+
+            var transparent = Mathf.Approximately(color.a, 0);
+            if(transparent != Mathf.Approximately(other.color.a, 0))
+            {
+                return false;
+            }
+
+            if (!transparent && (!Mathf.Approximately(color.r, other.color.r) || !Mathf.Approximately(color.g, other.color.g) ||
+                                 !Mathf.Approximately(color.b, other.color.b) || !Mathf.Approximately(color.a, other.color.a)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+    
+    public struct ImageFill : IEquatable<ImageFill>
+    {
+        public LinearFill? linear;
+        public Radial90Fill? radial90;
+        public Radial180Fill? radial180;
+        public Radial360Fill? radial360;
+
+        public float amount;
+
+        public bool Equals(ImageFill other)
+        {
+            if (linear.HasValue)
+            {
+                if (!other.linear.HasValue || !linear.Value.Equals(other.linear.Value))
+                {
+                    return false;
+                }
+            } else if (radial90.HasValue)
+            {
+                if (!other.radial90.HasValue || !radial90.Value.Equals(other.radial90.Value))
+                {
+                    return false;
+                }
+            }  if (radial180.HasValue)
+            {
+                if (!other.radial180.HasValue || !radial180.Value.Equals(other.radial180.Value))
+                {
+                    return false;
+                }
+            }  if (radial360.HasValue)
+            {
+                if (!other.radial360.HasValue || !radial360.Value.Equals(other.radial360.Value))
+                {
+                    return false;
+                }
+            } 
+
+            return Mathf.Approximately(amount, other.amount);
+        }
+    }
+
+    public struct LinearFill : IEquatable<LinearFill>
+    {
+        public enum Origin
+        {
+            Top,
+            Right,
+            Bottom,
+            Left
+        }
+
+        public Origin origin;
+
+        public bool Equals(LinearFill other) => origin == other.origin;
+    }
+
+    public struct Radial90Fill : IEquatable<Radial90Fill>
+    {
+        public enum Origin
+        {
+            TopRight,
+            BottomRight,
+            BottomLeft,
+            TopLeft
+        }
+
+        public Origin origin;
+        public bool clockwise;
+
+        public bool Equals(Radial90Fill other) => clockwise == other.clockwise && origin == other.origin;
+    }
+
+    public struct Radial180Fill : IEquatable<Radial180Fill>
+    {
+        public enum Origin
+        {
+            Top,
+            Right,
+            Bottom,
+            Left
+        }
+
+        public Origin origin;
+        public bool clockwise;
+
+        public bool Equals(Radial180Fill other) => clockwise == other.clockwise && origin == other.origin;
+    }
+
+    public struct Radial360Fill : IEquatable<Radial360Fill>
+    {
+        public enum Origin
+        {
+            Top,
+            Right,
+            Bottom,
+            Left
+        }
+
+        public Origin origin;
+        public bool clockwise;
+
+        public bool Equals(Radial360Fill other) => clockwise == other.clockwise && origin == other.origin;
     }
 }
--- Runtime/UnityComponents/UnityImageDefinition.cs
+++ Runtime/UnityComponents/UnityImageDefinition.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using RishUI.Components;
+using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
 namespace RishUI.UnityComponents
 {
     public struct UnityImageDefinition
     {
-        public enum Type { Simple, Sliced, Tiled }
+        public enum Type { Simple, Sliced, Tiled, Filled }
         
         public bool enabled;
         public Sprite sprite;
@@ -13,6 +14,7 @@ namespace RishUI.UnityComponents
         public bool raycastTarget;
         public bool maskable;
         public Type type;
+        public ImageFill fill;
         public bool preserveAspectRatio;
 
         public void SetComponent(Image imageComponent)
@@ -37,6 +39,114 @@ namespace RishUI.UnityComponents
                         break;
                     case Type.Tiled:
                         unityType = Image.Type.Tiled;
+                        break;
+                    case Type.Filled:
+                        var valid = false;
+                        if (fill.linear.HasValue)
+                        {
+                            var linear = fill.linear.Value;
+                            switch (linear.origin)
+                            {
+                                case LinearFill.Origin.Top:
+                                    imageComponent.fillMethod = Image.FillMethod.Vertical;
+                                    imageComponent.fillOrigin = 1;
+                                    break;
+                                case LinearFill.Origin.Right:
+                                    imageComponent.fillMethod = Image.FillMethod.Horizontal;
+                                    imageComponent.fillOrigin = 1;
+                                    break;
+                                case LinearFill.Origin.Bottom:
+                                    imageComponent.fillMethod = Image.FillMethod.Vertical;
+                                    imageComponent.fillOrigin = 0;
+                                    break;
+                                case LinearFill.Origin.Left:
+                                    imageComponent.fillMethod = Image.FillMethod.Horizontal;
+                                    imageComponent.fillOrigin = 0;
+                                    break;
+                                default:
+                                    throw new UnityException("Origin type not supported");
+                            }
+                            valid = true;
+                        } else if (fill.radial90.HasValue)
+                        {
+                            var radial90 = fill.radial90.Value;
+
+                            imageComponent.fillMethod = Image.FillMethod.Radial90;
+                            switch (radial90.origin)
+                            {
+                                case Radial90Fill.Origin.TopRight:
+                                    imageComponent.fillOrigin = 2;
+                                    break;
+                                case Radial90Fill.Origin.BottomRight:
+                                    imageComponent.fillOrigin = 3;
+                                    break;
+                                case Radial90Fill.Origin.BottomLeft:
+                                    imageComponent.fillOrigin = 0;
+                                    break;
+                                case Radial90Fill.Origin.TopLeft:
+                                    imageComponent.fillOrigin = 1;
+                                    break;
+                                default:
+                                    throw new UnityException("Origin type not supported");
+                            }
+                            imageComponent.fillClockwise = radial90.clockwise;
+                            
+                            valid = true;
+                        } else if (fill.radial180.HasValue)
+                        {
+                            var radial180 = fill.radial180.Value;
+
+                            imageComponent.fillMethod = Image.FillMethod.Radial180;
+                            switch (radial180.origin)
+                            {
+                                case Radial180Fill.Origin.Top:
+                                    imageComponent.fillOrigin = 2;
+                                    break;
+                                case Radial180Fill.Origin.Right:
+                                    imageComponent.fillOrigin = 3;
+                                    break;
+                                case Radial180Fill.Origin.Bottom:
+                                    imageComponent.fillOrigin = 0;
+                                    break;
+                                case Radial180Fill.Origin.Left:
+                                    imageComponent.fillOrigin = 1;
+                                    break;
+                                default:
+                                    throw new UnityException("Origin type not supported");
+                            }
+                            imageComponent.fillClockwise = radial180.clockwise;
+                            
+                            valid = true;
+                        } else if (fill.radial360.HasValue)
+                        {
+                            var radial360 = fill.radial360.Value;
+
+                            imageComponent.fillMethod = Image.FillMethod.Radial360;
+                            switch (radial360.origin)
+                            {
+                                case Radial360Fill.Origin.Top:
+                                    imageComponent.fillOrigin = 2;
+                                    break;
+                                case Radial360Fill.Origin.Right:
+                                    imageComponent.fillOrigin = 1;
+                                    break;
+                                case Radial360Fill.Origin.Bottom:
+                                    imageComponent.fillOrigin = 0;
+                                    break;
+                                case Radial360Fill.Origin.Left:
+                                    imageComponent.fillOrigin = 3;
+                                    break;
+                                default:
+                                    throw new UnityException("Origin type not supported");
+                            }
+                            imageComponent.fillClockwise = radial360.clockwise;
+                            
+                            valid = true;
+                        }
+
+                        imageComponent.fillAmount = fill.amount;
+                        
+                        unityType = valid ? Image.Type.Filled : Image.Type.Simple;
                         break;
                     default:
                         throw new UnityException("Image type not supported");
