2d9967852c31f51881519f2fc91e7a8000d5e5d0
Lisandro Crespo <hey@lisandroct.com>
2023-08-02 10:53:39 -0500

    Update

--- Runtime/CreateChildren.cs
+++ Runtime/CreateChildren.cs
@@ -339,6 +339,336 @@ namespace RishUI
             
             return Create(array);
         }
+        public static Children Children(Element child0)
+        {
+            var array = new NativeArray<Children>(1, Allocator.Persistent);
+            array[0] = child0;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1)
+        {
+            var array = new NativeArray<Children>(2, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2)
+        {
+            var array = new NativeArray<Children>(3, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3)
+        {
+            var array = new NativeArray<Children>(4, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4)
+        {
+            var array = new NativeArray<Children>(5, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5)
+        {
+            var array = new NativeArray<Children>(6, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6)
+        {
+            var array = new NativeArray<Children>(7, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7)
+        {
+            var array = new NativeArray<Children>(8, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8)
+        {
+            var array = new NativeArray<Children>(9, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8, Element child9)
+        {
+            var array = new NativeArray<Children>(10, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            array[9] = child9;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8, Element child9, Element child10)
+        {
+            var array = new NativeArray<Children>(11, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            array[9] = child9;
+            array[10] = child10;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8, Element child9, Element child10, Element child11)
+        {
+            var array = new NativeArray<Children>(12, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            array[9] = child9;
+            array[10] = child10;
+            array[11] = child11;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8, Element child9, Element child10, Element child11, Element child12)
+        {
+            var array = new NativeArray<Children>(13, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            array[9] = child9;
+            array[10] = child10;
+            array[11] = child11;
+            array[12] = child12;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8, Element child9, Element child10, Element child11, Element child12, Element child13)
+        {
+            var array = new NativeArray<Children>(14, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            array[9] = child9;
+            array[10] = child10;
+            array[11] = child11;
+            array[12] = child12;
+            array[13] = child13;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8, Element child9, Element child10, Element child11, Element child12, Element child13, Element child14)
+        {
+            var array = new NativeArray<Children>(15, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            array[9] = child9;
+            array[10] = child10;
+            array[11] = child11;
+            array[12] = child12;
+            array[13] = child13;
+            array[14] = child14;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8, Element child9, Element child10, Element child11, Element child12, Element child13, Element child14, Element child15)
+        {
+            var array = new NativeArray<Children>(16, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            array[9] = child9;
+            array[10] = child10;
+            array[11] = child11;
+            array[12] = child12;
+            array[13] = child13;
+            array[14] = child14;
+            array[15] = child15;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8, Element child9, Element child10, Element child11, Element child12, Element child13, Element child14, Element child15, Element child16)
+        {
+            var array = new NativeArray<Children>(17, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            array[9] = child9;
+            array[10] = child10;
+            array[11] = child11;
+            array[12] = child12;
+            array[13] = child13;
+            array[14] = child14;
+            array[15] = child15;
+            array[16] = child16;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8, Element child9, Element child10, Element child11, Element child12, Element child13, Element child14, Element child15, Element child16, Element child17)
+        {
+            var array = new NativeArray<Children>(18, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            array[9] = child9;
+            array[10] = child10;
+            array[11] = child11;
+            array[12] = child12;
+            array[13] = child13;
+            array[14] = child14;
+            array[15] = child15;
+            array[16] = child16;
+            array[17] = child17;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8, Element child9, Element child10, Element child11, Element child12, Element child13, Element child14, Element child15, Element child16, Element child17, Element child18)
+        {
+            var array = new NativeArray<Children>(19, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            array[9] = child9;
+            array[10] = child10;
+            array[11] = child11;
+            array[12] = child12;
+            array[13] = child13;
+            array[14] = child14;
+            array[15] = child15;
+            array[16] = child16;
+            array[17] = child17;
+            array[18] = child18;
+            
+            return Create(array);
+        }
+        public static Children Children(Element child0, Element child1, Element child2, Element child3, Element child4, Element child5, Element child6, Element child7, Element child8, Element child9, Element child10, Element child11, Element child12, Element child13, Element child14, Element child15, Element child16, Element child17, Element child18, Element child19)
+        {
+            var array = new NativeArray<Children>(20, Allocator.Persistent);
+            array[0] = child0;
+            array[1] = child1;
+            array[2] = child2;
+            array[3] = child3;
+            array[4] = child4;
+            array[5] = child5;
+            array[6] = child6;
+            array[7] = child7;
+            array[8] = child8;
+            array[9] = child9;
+            array[10] = child10;
+            array[11] = child11;
+            array[12] = child12;
+            array[13] = child13;
+            array[14] = child14;
+            array[15] = child15;
+            array[16] = child16;
+            array[17] = child17;
+            array[18] = child18;
+            array[19] = child19;
+            
+            return Create(array);
+        }
         public static Children Children(params Children[] children)
         {
             var length = children?.Length ?? 0;
