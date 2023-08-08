b267bbe94623b4123e17f9a23b51dc82937444a3
Lisandro Crespo <hey@lisandroct.com>
2023-08-08 08:35:22 -0500

    Remove unnecessary logs

--- Runtime/Rish.cs
+++ Runtime/Rish.cs
@@ -181,8 +181,6 @@ namespace RishUI
                 return;
             }
             
-            Debug.Log($"Register {typeof(T)}({id}) to {owner.GetType()}");
-            
             GetPool<T>()?.RegisterReference(id, owner);
         }
         public static void UnregisterReferenceTo<T>(uint id, IOwner owner) where T : class, IManaged
@@ -191,8 +189,6 @@ namespace RishUI
             {
                 return;
             }
-            
-            Debug.Log($"Unregister {typeof(T)}({id}) from {owner.GetType()}");
             
             GetPool<T>()?.UnregisterReference(id, owner);
         }
@@ -211,7 +207,6 @@ namespace RishUI
             }
             
             var pool = PoolsList[poolIndex];
-            Debug.Log($"Register {pool.GetType()}({id}) to {owner.GetType()}");
             pool.RegisterReference(id, owner);
         }
         public static void UnregisterReferenceTo(Reference reference, IOwner owner)
@@ -229,7 +224,6 @@ namespace RishUI
             }
 
             var pool = PoolsList[poolIndex];
-            Debug.Log($"Unregister {pool.GetType()}({id}) from {owner.GetType()}");
             pool.UnregisterReference(id, owner);
         }
 
