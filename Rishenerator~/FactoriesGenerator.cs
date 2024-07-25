37111a3cb1565ae2e5efa05484d0e664b61e1ead
Lisandro Crespo <hey@lisandroct.com>
2024-07-25 09:17:45 -0500

    Allow adding IEnumerables to RishList

--- Rishenerator~/FactoriesGenerator.cs
+++ Rishenerator~/FactoriesGenerator.cs
@@ -767,7 +767,6 @@ namespace Rishenerator
                         
                             if (interfaceTypeFullName == validOverridableInterface)
                             {
-                                Logger.Log($"This is it! {nestedTypeSymbol.GetFullName(true)}");
                                 return nestedTypeSymbol.GetFullName(true);
                             }
                         }
